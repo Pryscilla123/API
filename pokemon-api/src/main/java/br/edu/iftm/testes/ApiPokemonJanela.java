5f44f21a8bea309be7b1eebce876567af810e8c4
Pryscilla <pryscillaromao@hotmail.com>
2020-07-08 19:45:15 -0300

    Api manipulada

--- pokemon-api/src/main/java/br/edu/iftm/testes/ApiPokemonJanela.java
+++ pokemon-api/src/main/java/br/edu/iftm/testes/ApiPokemonJanela.java
@@ -1,50 +1,183 @@
 package br.edu.iftm.testes;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.WindowConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
-public class ApiPokemonJanela {
-	public static void criarJanela {
-		Jframe janela = new JFrame();
-		
-		janela.setBounds(250, 100, 400, 400);
-		janela.setLayout(null);
-		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		janela.setVisible(true);
-		janela.setResizable(false);
-		
-		JPanel panelEsquerda = new JPanel();
-		panelEsquerda.setBackground(Color.WHITE);
-		panelEsquerda.setBounds(140, 0, 250, 365);
-		panelEsquerda.setLayout(null)
-		janela.add(panelEsquerda);
-		
-		JPanel panelDireita = new JPanel();
-		panelDireita.setBackground(SystemColor.inactiveCaption);
-		panelDireita.setBounds(0, 0, 142, 365);
-		panelDireita.setLayout(null);
-		janela.add(panelDireita);
-		
-		JLabel label = new JLabel("Nome do pokemon:");
-		label.setBounds(10, 28, 122, 20);
-		panelDireita.add(label);
-		
-		JTextField campoTexto = new JTextField();
-		campoTexto.setBounds(10, 59, 122, 20);
-		panelDireita.add(campoTexto);
-		campoTexto.setColumns(10);
-		
-		JButton botao = new JButton("PESQUISAR");
-		botao.setBackground(Color.WHITE);
-		botao.setForeground(Color.BLACK);
-		botao.setBounds(10, 90, 120, 23);
-		panelDireita.add(botao);
-		
+import javax.imageio.ImageIO;
+import javax.swing.ComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import br.edu.iftm.api.Api;
+import br.edu.iftm.api.JsonParser;
+
+public class ApiPokemonJanela extends JPanel{
+	
+	private String nomePokemon;
+	private String url;
+	private String spritePokemon;
+	private String typePokemon;
+	private ArrayList<String> abilities = new ArrayList<String>();
+	private int idPokemon;
+	private Api api;
+	private JSONObject json;
+	private JPanel painelEsquerda;
+	private JPanel painelDireita;
+	private JLabel label;
+	private JTextField caixaTexto;
+	private JTextArea pokedex;
+	private JLabel imagemPokemon;
+	private JComboBox pokemons;
+	private int count;
+	
+	public ApiPokemonJanela() throws IOException {
+		//nomePokemon = "bubasaur";
+		setBounds(0, 0, 700, 500);
+		setLayout(null);
+		janela();
 	}
 	
+	public void janela() throws MalformedURLException, IOException {
+		
+		pokemons = new JComboBox(listaPokemon());
+		pokemons.setBounds(50, 80, 122, 20);
+		pokemons.setEditable(false);
+		pokemons.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				nomePokemon = pokemons.getSelectedItem().toString();
+				try {
+					remove(imagemPokemon);
+					revalidate();
+					repaint();
+					
+					pokedex.setText("");
+					pokedex(nomePokemon);
+					
+					imagemPokemon = pegarIcone(idPokemon);
+					add(imagemPokemon);
+					
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		label = new JLabel("Nome do Pokémon:");
+		label.setFont(new Font("Tahoma", Font.BOLD, 12));
+		label.setBounds(50, 28, 122, 60);
+		
+		imagemPokemon = pegarIcone(idPokemon);
+		
+		pokedex = new JTextArea();
+		pokedex.setEditable(false);
+		pokedex.setBounds(300, 200, 380, 260);
+		
+		add(pokemons);
+		add(imagemPokemon);
+		add(pokedex);
+		//add(caixaTexto);
+		add(label);
+		setVisible(true);
+	}
+	
+	private void pokedex(String nomePokemon) throws IOException {
+		
+		String url = String.format("https://pokeapi.co/api/v2/pokemon/%s", nomePokemon);
+		api = new Api(url);
+		json = JsonParser.parseToObject(api.executar());
+		
+		//Id
+		idPokemon = json.getInt("id");
+		pokedex.append("Id do Pokémon: " + idPokemon + "\n");
+		
+		pokedex.append("\n");
+		
+		//Tipo
+		pokedex.append("Tipo do Pokémon\n");
+		JSONArray tipos = json.getJSONArray("types");
+		
+		for(int i = 0; i < tipos.length(); i ++) {
+			JSONObject allTypes = tipos.getJSONObject(i);
+			JSONObject type = allTypes.getJSONObject("type");
+			pokedex.append("-" + type.getString("name") + "\n");
+		}
+		
+		pokedex.append("\n");
+		
+		//Habilidades do Pokémon
+		pokedex.append("Habilidades do Pokémon\n");
+		
+		JSONArray abilities = json.getJSONArray("abilities");
+		
+		for(int i = 0; i < abilities.length(); i++) {
+			JSONObject abilityCompleta = abilities.getJSONObject(i);
+			JSONObject ability = abilityCompleta.getJSONObject("ability");
+			pokedex.append("-" + ability.getString("name") + "\n");
+		}
+	}
+	
+	private String[] listaPokemon() throws IOException{
+		
+		String url = "https://pokeapi.co/api/v2/pokemon/";
+		api = new Api(url);
+		json = JsonParser.parseToObject(api.executar());
+		int count = json.getInt("count");
+		
+		String nomes[] = new String[count];
+		
+		url = String.format("https://pokeapi.co/api/v2/pokemon?offset=0&limit=%d", count);
+		api = new Api(url);
+		json = JsonParser.parseToObject(api.executar());
+		
+		JSONArray results = json.getJSONArray("results");
+		
+		for(int i = 0; i < results.length(); i++) {
+			JSONObject nomesPoke = results.getJSONObject(i);
+			nomes[i] = nomesPoke.getString("name");
+		}
+		
+		return nomes;
+	}
+	
+	private static JLabel pegarIcone(int fotoId) throws MalformedURLException, IOException {
+		String fotoUrl = String.format("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%d.png", fotoId);
+		JLabel icone = new JLabel("");
+		Image imagem = ImageIO.read(new URL(fotoUrl));
+		int larguraImagem = imagem.getWidth(null);
+		int alturaImagem = imagem.getHeight(null);
+		icone.setIcon(new ImageIcon(imagem));
+		icone.setBounds(450, 100, larguraImagem, alturaImagem);
+		return icone;
+	}
+
+	public int getIdPokemon() {
+		return idPokemon;
+	}
+
+	public void setIdPokemon(int idPokemon) {
+		this.idPokemon = idPokemon;
+	}
+
 }
--- pokemon-api/src/main/java/br/edu/iftm/testes/ApiPokemonTeste.java
+++ pokemon-api/src/main/java/br/edu/iftm/testes/ApiPokemonTeste.java
@@ -1,10 +1,20 @@
 package br.edu.iftm.testes;
 
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
 public class ApiPokemonTeste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		JFrame pokeTela = new JFrame();
+		pokeTela.setBounds(250, 100, 700, 500);
+		pokeTela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pokeTela.setVisible(true);
+		
+		ApiPokemonJanela poke = new ApiPokemonJanela();
+		pokeTela.add(poke);
 	}
 
 }
